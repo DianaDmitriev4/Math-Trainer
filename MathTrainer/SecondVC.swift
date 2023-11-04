970aaca5ac2b3a542f88e125ad690701b6bb52b2
DianaDmitriev4 <qwertyuiop25852@mail.ru>
2023-11-04 22:08:19 +0700

    change button "back" and add shadow (absolutely the same as on the first VC)

--- MathTrainer/SecondVC.swift
+++ MathTrainer/SecondVC.swift
@@ -11,7 +11,7 @@ import UIKit
 
 final class SecondVC: UIViewController {
     //MARK: - IBOutlet
-    
+    @IBOutlet var answerButtons: [UIButton]!
     
     //MARK: - Properties
     var type: MathType = .add
@@ -19,8 +19,16 @@ final class SecondVC: UIViewController {
     //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        addShadow()
     }
     
     //MARK: - Methods
-    
+    private func addShadow() {
+            answerButtons.forEach { button in
+                button.layer.shadowColor = UIColor.darkGray.cgColor
+                button.layer.shadowOffset = CGSize(width: 0, height: 2)
+                button.layer.shadowOpacity = 0.4
+                button.layer.shadowRadius = 3
+            }
+        }
 }
