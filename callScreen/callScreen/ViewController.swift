fcb818c3b794efd1fddcbd6857d6e19684776444
DianaDmitriev4 <qwertyuiop25852@mail.ru>
2023-11-08 00:14:23 +0700

    Add homework with call screen

---
+++ callScreen/callScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  callScreen
+//
+//  Created by User on 03.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
